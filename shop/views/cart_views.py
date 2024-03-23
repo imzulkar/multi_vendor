cd065e3aa9f11b9a52ed034278fd5c8ee35a0117
imzulkar <gmzulkar@gmail.com>
2024-03-24 00:39:32 +0600

    fix: card shape changed
    Signed-off-by: imzulkar <gmzulkar@gmail.com>

--- shop/views/cart_views.py
+++ shop/views/cart_views.py
@@ -3,19 +3,17 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy
 
-from inventory.models import Products
 
-
-@login_required(login_url='{% url "user:auth" %}')
 def shopping_view(request):
-    user = request.user
-    if user.is_authenticated:
-        if request.user.user_type == "BUYER":
-            return render(request, 'cart.html')
-
-        return HttpResponseRedirect(reverse_lazy('shop:shopping_template'))
-
-    return HttpResponseRedirect(reverse_lazy('user:auth'))
+    return render(request, 'cart.html')
+    # user = request.user
+    # if user.is_authenticated:
+    #     if request.user.user_type == "BUYER":
+    #         return render(request, 'cart.html')
+    #
+    #     return HttpResponseRedirect(reverse_lazy('shop:shopping_template'))
+    #
+    # return HttpResponseRedirect(reverse_lazy('user:auth'))
 
 
 @login_required(login_url='{% url "user:auth" %}')
--- shop/views/index_views.py
+++ shop/views/index_views.py
@@ -7,6 +7,6 @@ def index_view(request):
     if user.is_authenticated:
         if request.user.user_type == "SELLER":
             return HttpResponseRedirect(reverse_lazy('shop:products_list_template'))
-        return HttpResponseRedirect(reverse_lazy('shop:shopping_template'))
+    return HttpResponseRedirect(reverse_lazy('shop:shopping_template'))
 
-    return HttpResponseRedirect(reverse_lazy('user:auth'))
+    # return HttpResponseRedirect(reverse_lazy('user:auth'))
